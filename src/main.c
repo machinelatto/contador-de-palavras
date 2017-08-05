505a66cf84e6d5a065f6c965f6900bc1aa2979d2
machinelatto <machinelatto@gmail.com>
2017-08-05 18:42:08 -0300

    Contador funcionando, passou em todos os testes

--- src/main.c
+++ src/main.c
@@ -1,22 +1,20 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
-
 #include <stdio.h>
-
-int main() {
-
-  char c;
-
-  c = 1;
-
-  while (c != '\n') {
-    scanf("%c", &c);
-  }
-
-  printf("1\n");
-  return 0;
+#include<string.h>
+int main(){
+int n, num_palavras=0, i;
+char st[1001];
+fgets(st,1000,stdin);
+n=strlen(st); //descubro o tamanho da string
+for(i=0;i<n;i++){ //percorro essa string
+    while(st[i]==' ' || st[i]=='.' || st[i]==',' || st[i]=='-' || st[i]=='!')  //continuo enquanto nao encontro palavras
+        i++; 
+    if(st[i]!='\n') //Se achou uma palavra
+        num_palavras++;                      //incrementa numero de palavras
+    while(st[i]!=' ' && st[i] != '\n' && st[i]!='.' && st[i]!=',' && st[i]!='-')//passa pela palavra
+    	i++;
+    if((st[i] == '.' || st[i] == ',') && (st[i+1] >= 48 && st[i+1]<=57))
+    	num_palavras--;
+} 
+printf("%d\n", num_palavras);//j conta o numero de palavras
+return(0);
 }

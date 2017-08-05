f15cce303d04c22463a0b58f3040633929418636
machinelatto <machinelatto@gmail.com>
2017-08-05 19:17:24 -0300

    Novos comentários adicionados

--- src/main.c
+++ src/main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
 int main(){
 int n, num_palavras=0, i;
 char st[1001];
-fgets(st,1000,stdin);
+fgets(st,1000,stdin); //usei fgets() em vez de scanf() e funcionou, espero que aceite :)
 n=strlen(st); //descubro o tamanho da string
 for(i=0;i<n;i++){ //percorro essa string
-    while(st[i]==' ' || st[i]=='.' || st[i]==',' || st[i]=='-' || st[i]=='!')  //continuo enquanto nao encontro palavras
+    while(st[i]==' ' || st[i]=='.' || st[i]==',' || st[i]=='-' || st[i]=='!')  //continuo enquanto nao encontro o começo de uma palavra
         i++; 
-    if(st[i]!='\n') //Se achou uma palavra
-        num_palavras++;                      //incrementa numero de palavras
-    while(st[i]!=' ' && st[i] != '\n' && st[i]!='.' && st[i]!=',' && st[i]!='-')//passa pela palavra
+    if(st[i]!='\n') //Se achar uma palavra, sai do while e entra aqui incrementando o valor
+        num_palavras++;                     
+    while(st[i]!=' ' && st[i] != '\n' && st[i]!='.' && st[i]!=',' && st[i]!='-') //percorre a palavra, '.,- ' indicam o final de uma palavra 
     	i++;
-    if((st[i] == '.' || st[i] == ',') && (st[i+1] >= 48 && st[i+1]<=57))
-    	num_palavras--;
+    if((st[i] == '.' || st[i] == ',') && (st[i+1] >= 48 && st[i+1]<=57)) //a lógica utilizada iria contar números com vírgula ou ponto em dobro
+    	num_palavras--;                                                  //assim se o o ponto ou virgula estiver num numero subtrai um do total
 } 
-printf("%d\n", num_palavras);//j conta o numero de palavras
+printf("%d\n", num_palavras);//imprime o resultado
 return(0);
 }
